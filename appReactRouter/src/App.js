dcd5993700246b8bf210bcb187854463a5247e4a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-03 02:20:49 -0200

    Reorganização dos projetos desenvolvidos durante o estudo de React + Flux7

---
+++ appReactRouter/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import PlacarApp from './components/PlacarApp.js';
+
+ReactDOM.render(
+	
+    <PlacarApp />,
+
+    document.getElementById("app")
+);
+
