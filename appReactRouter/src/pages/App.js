e207edb17664151af4eb5832636fce9094050aa3
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-03 21:23:02 -0200

    Alteração no arquivo index.html

--- appReactRouter/src/pages/App.js
+++ appReactRouter/src/pages/App.js
@@ -8,8 +8,20 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Testando um Componente por meio de Rotas....</h1>
-			</div>
+				<nav>
+					<div>
+						<a href="#" className="nav-item">Página Principal</a>
+						<a href="#" className="nav-item">Cursos</a>
+						<a href="#" className="nav-item">Sobre</a>
+					</div>
+				</nav>
+
+				<section className="section">
+					<div className="container">
+						<h1 className="title">Monstrando componente através do Router</h1>
+					</div>
+				</section>
+			<div>
 		);
 	}
 }
