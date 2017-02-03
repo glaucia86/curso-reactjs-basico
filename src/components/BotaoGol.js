6431e315f2dd2a29d39e2ce0c405ee39626b2ed4
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-02 23:45:02 -0200

    Alterações nos arquivos: BotaoGol.js & Partida.js

---
+++ src/components/BotaoGol.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class BotaoGol extends React.Component {
+    render() {
+        return (
+            <div>
+            </div>       
+        );
+    }
+}
--- src/components/Partida.js
+++ src/components/Partida.js
@@ -4,6 +4,12 @@ export default class Partida extends React.Component {
     render() {
         return (
             <div>
+                <h2>Estádio</h2>
+                <div>
+                    <span>Data</span>
+                    <span>-</span>
+                    <span>Hora</span>
+                </div>
             </div>       
         );
     }
