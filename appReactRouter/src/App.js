f3c76f6b4bb54ed8409829c37b8620a4663ab560
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-03 22:59:43 -0200

    Criação de um novo componente 'Home.js' e a definição do componente como IndexRouter da página.

--- appReactRouter/src/App.js
+++ appReactRouter/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App from './pages/App';
 import Sobre from './pages/Sobre';
 import Cursos from './pages/Cursos';
+import Home from './pages/Home'
 
 ReactDOM.render((
 		<Router history={hashHistory}>
 			<Route path="/" component={App}>
+				<IndexRoute component={Home} />
 				<Route path="/cursos" component={Cursos}/>
-				<Route path="/sobre" component={Sobre}/>
+				<Route path="/sobre" component={Sobre}/>				
 			</Route>
 		</Router>
 	),
